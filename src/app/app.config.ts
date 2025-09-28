45f23304943b244610626fe437df376e995bd120
donovangaffney <donovangaffney@gmail.com>
2025-09-27 23:14:45 -0400

    Fix styles

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {InMemoryScrollingOptions, provideRouter, withInMemoryScrolling} from '@angular/router';
 import { routes } from './app.routes';
 import {provideHttpClient} from '@angular/common/http';
+
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'top',
+  anchorScrolling: 'enabled', // Optional: enables scrolling to anchors based on fragment identifiers
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling(scrollConfig)),
     provideHttpClient()
   ]
 };
